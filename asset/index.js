fa5e9412c6ae34269d68bfa3e6b2acd0306a156f
shrotavre <avreghly@gmail.com>
2019-02-26 14:57:11 +0700

    Remake source code

--- asset/index.js
+++ asset/index.js
@@ -1,58 +1,104 @@
-// UI Elements
-var button = document.getElementById('button');
-var player = document.getElementById('player');
+class StreamToChunkRecorder {
+  constructor(stream) {
+    this.mediaRecorder = stream ? new MediaRecorder(stream) : null
+  }
 
-var state = 'standby';
-var blob;
-var chunks = [];
+  attach({ stream }) {
+    this.mediaRecorder = new MediaRecorder(stream)
+  }
 
-if (navigator.mediaDevices.getUserMedia) {
-    navigator.mediaDevices.getUserMedia({ audio: true }).then(function (stream) {
-        var mediaRecorder = new MediaRecorder(stream);
+  record(writerFunction) {
+    if (!this.mediaRecorder) throw new Error("No stream attached!")
 
-        button.onclick = function () {
-            switch (state) {
-                case 'standby':
-                    mediaRecorder.start();
+    this.mediaRecorder.ondataavailable = writerFunction
+    this.mediaRecorder.start()
+  }
 
-                    button.innerHTML = 'Stop';
-                    state = 'recording';
-                    break;
-                case 'recording':
-                    mediaRecorder.stop();
+  stop() {
+    this.mediaRecorder.stop()
+  }
+}
 
-                    button.innerHTML = '...';
-                    state = 'resounding';
-                    break;
-            }
-        }
+class State {
+  constructor(value) {
+    this.state = value
+    this.listeners = []
+  }
 
-        player.addEventListener('ended', function () {
-            button.innerHTML = 'Record';
-            state = 'standby';
-        });
+  change(value) {
+    this.state = value
 
-        mediaRecorder.onstop = function (e) {
-            resound();
-        }
-
-        mediaRecorder.ondataavailable = function (e) {
-            chunks.push(e.data);
-        }
-    }, function (err) {
-        console.log('The following error occured: ' + err);
+    // dispatch listeners
+    this.listeners.forEach(listenerFunction => {
+      listenerFunction(this.state)
     });
+  }
 
-} else {
-    console.log('getUserMedia not supported on your browser!');
+  now() {
+    return this.state
+  }
+
+  onChange(listenerFunction) {
+    this.listeners.push(listenerFunction)
+  }
 }
 
-function resound() {
-    blob = new Blob(chunks, { 'type': 'audio/ogg; codecs=opus' });
-    var audioURL = window.URL.createObjectURL(blob);
+// MAIN ROUTINE
+const controlButtonElement = document.getElementById('button')
+const audioPlayerElement = document.getElementById('player')
+const stateDisplayElement = document.getElementById('state-displayer')
 
-    chunks = [];
+const streamRecorder = new StreamToChunkRecorder()
+var appState = new State('idle')
+var recorderChunks = []
 
-    player.src = audioURL;
-    player.play();
-}
+if (!navigator.mediaDevices.getUserMedia) throw new Error('Web recording is not supported/enabled. Please update your browser!')
+
+navigator.mediaDevices.getUserMedia({ audio: true }).then(stream => streamRecorder.attach({ stream }))
+
+appState.onChange(newState => {
+  switch (newState) {
+    case 'idle':
+      audioPlayerElement.pause();
+      audioPlayerElement.currentTime = 0;
+
+      stateDisplayElement.innerHTML = 'idle'
+      controlButtonElement.innerHTML = 'record'
+      break
+    case 'recording':
+      stateDisplayElement.innerHTML = 'recording...'
+      controlButtonElement.innerHTML = 'echo';
+      break
+    case 'playing':
+      stateDisplayElement.innerHTML = 'playing...'
+      controlButtonElement.innerHTML = 'stop';
+      break
+    case 'play-ready':
+      appState.change('playing')
+      audioPlayerElement.play()
+      break
+  }
+})
+
+audioPlayerElement.addEventListener('ended', () => appState.change('idle'))
+controlButtonElement.addEventListener('click', () => {
+  switch (appState.now()) {
+    case 'idle':
+      appState.change('recording')
+      streamRecorder.record(e => {
+        const blob = new Blob([e.data], { 'type': 'audio/ogg; codecs=opus' });
+        const audioURL = window.URL.createObjectURL(blob);
+        recorderChunks = [];
+
+        audioPlayerElement.src = audioURL;
+        appState.change('play-ready')
+      })
+      break
+    case 'recording':
+      streamRecorder.stop()
+      break
+    case 'playing':
+      appState.change('idle')
+      break
+  }
+})

1604eac63d626371d625e7962218251566b05893
Avre Barra <avreghly@gmail.com>
2022-01-30 13:24:19 +0700

    update deps for remote deploy

--- src/config.tsx
+++ src/config.tsx
@@ -3,7 +3,7 @@ const CONFIG = {
   appDescription: "Mirror your voice!",
   appFaviconURL:
     "https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/120/joypixels/291/parrot_1f99c.png",
-  subDirPath: process.env.SUB_DIR_PATH || "",
+  subDirPath: process.env.DISABLE_GITHUB_SUBDIR_PATH ? "" : "echodio",
   errors: {
     ErrRecorderNotReady: {
       title: "Recorder Not Ready",
